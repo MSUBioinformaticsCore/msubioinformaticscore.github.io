f4b5e4207911df8ea1adb20a829baba70b3dad9e
John Vusich <82066832+johnvusich@users.noreply.github.com>
2025-06-17 16:28:54 -0400

    Create run_differential_atac.sh

---
+++ scripts/run_differential_atac.sh
@@ -0,0 +1,40 @@
+#!/bin/bash --login
+#SBATCH --job-name=differential
+#SBATCH --time=3:00:00
+#SBATCH --mem=4GB
+#SBATCH --cpus-per-task=1
+#SBATCH --output=differential-%j.out
+
+# Load Nextflow
+module purge
+module load Nextflow
+
+# Set the relative paths to the genome files
+GENOME_DIR="/mnt/research/common-data/Bio/genomes/Ensembl_GRCm39_mm39" #Example reference genome
+FASTA="$GENOME_DIR/genome.fa" # Example FASTA
+GTF="$GENOME_DIR/genes.gtf" # Example GTF
+
+# Define the samplesheet, outdir, workdir, and config
+SAMPLESHEET="$HOME/differential/samplesheet.csv" # Replace with path to sample sheet
+MATRIX="$HOME/atacseq/results/bwa/merged_replicate/macs2/narrow_peak/consensus/differentialabundance.atacseq.GRCh38.featureCount.tsv" # Example path to counts matrix
+FEATURES="$HOME/atacseq/results/bwa/merged_replicate/macs2/narrow_peak/consensus/differentialabundance.atacseq.GRCh38.annot.tsv" # Example path to gene lengths matrix
+CONTRASTS="$HOME/differential/contrasts.csv" # Example path to contrasts file
+OUTDIR="$HOME/differential/results" # Example path to results directory
+WORKDIR="$SCRATCH/differential/work" # Example path to work directory
+CONFIG="$HOME/atacseq/icer.config" # Example path to icer.config file
+
+# Run the pipeline
+nextflow pull nf-core/differentialabundance
+nextflow run nf-core/differentialabundance -r 1.5.0 -profile singularity -work-dir $WORKDIR -resume \
+--input $SAMPLESHEET \
+--matrix $MATRIX \
+--features $FEATURES \
+--features_metadata_cols "gene_id,gene_name" \
+--features_id_col gene_id \
+--features_name_col "gene_name" \
+--sizefactors_from_controls false \
+--contrasts $CONTRASTS \
+--outdir $OUTDIR \
+--fasta $FASTA \
+--gtf $GTF \
+-c $CONFIG
